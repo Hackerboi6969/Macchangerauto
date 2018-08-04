b08ca9974ce592de45bb8d78a78e262fa48b2492
apoctr <apoctr@tutanota.com>
2018-08-04 15:49:33 +0100

    retry

--- macchanger.py
+++ macchanger.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
+
 import subprocess
 import sys
-print("If you would like more updates and more things adding please feel free to donate to this Bitcoin address 14bnKzJWjYHYFndBuRbhth6aTbKGPjwmjg")
 
-network="eth0"
+def handle_args(argv):
+    """produce config based on passed arguments"""
+    config = { "network": "eth0" } #default config to return
+    for argument in argv:
+        if argument == "-w" or argument == "--wlan0": #backwards compatability
+            config["network"] = "wlan0"
+        elif argument == "-i" or argument == "--interface":
+            try:
+                config["network"] = next(argv)
+            except StopIteration: #if no interface was specified ("-i" is last arg)
+                print("Must provide an interface!", file=sys.stderr) #write to stderr
+                sys.exit(1)
+    return config
 
-for argument in sys.argv:
-	if argument == "-w":
-		network= "wlan0"
+def main():
+    #create an iterable so we can use next() then pass to handle_args
+    config = handle_args(iter(sys.argv))
+    network = config["network"]
 
-subprocess.call(f"ip link set {network} down", shell=True)
-subprocess.call(f"macchanger -r {network}", shell=True)
-subprocess.call(f"ip link set {network} up", shell=True)
-subprocess.call("systemctl restart NetworkManager", shell=True)
+    subprocess.call(f"ip link set {network} down", shell=True)
+    subprocess.call(f"macchanger -r {network}", shell=True)
+    subprocess.call(f"ip link set {network} up", shell=True)
+    subprocess.call("systemctl restart NetworkManager", shell=True)
 
-
-
+if __name__ == "__main__":
+    print("If you would like more updates and more things adding please feel free to donate to this Bitcoin address 14bnKzJWjYHYFndBuRbhth6aTbKGPjwmjg")
+    main()
